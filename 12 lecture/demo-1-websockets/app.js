b4feac94c140555158a142ae6d27559181892e03
sharonxylin <sl742041@gmail.com>
2021-11-09 11:17:54 -0800

    websocket demo

--- 12 lecture/demo-1-websockets/app.js
+++ 12 lecture/demo-1-websockets/app.js
@@ -1,13 +1,33 @@
 import fs from 'fs';
 import express from 'express';
+import enableWs from 'express-ws';
 
 const app = express();
+enableWs(app);
 
 app.get(['/', '/index.html'], (req, res) => {
   res.type('html')
   let cssContents = fs.readFileSync("index.html").toString();
   res.send(cssContents)
 })
+
+let allWebSockets = [];
+let socketCounter = 1;
+app.ws('/socket', (ws, req) =>{
+  console.log("a websocket opened");
+  allWebSockets.push(ws);
+  let myNum = socketCounter;
+   socketCounter++;
+  ws.on('message', msg =>{
+    allWebSockets.forEach(socket=>{
+      socket.send(myNum + ": " + msg);
+    });
+  });
+  ws.on('close', ()=>{
+    console.log("websocket " + myNum + " closed");
+  });
+
+});
 
 app.listen(3000, () => {
   console.log('Example app listening at http://localhost:3000')
